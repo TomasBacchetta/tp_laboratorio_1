66045c364e54c20944485b3025847703bd9f2753
TomasBacchetta <78171375+TomasBacchetta@users.noreply.github.com>
2021-06-18 01:14:56 -0300

    Delete parser.h

--- TP3/parser.h
+++
@@ -1,9 +0,0 @@
-#ifndef PARSER_H_INCLUDED
-#define PARSER_H_INCLUDED
-
-int parser_EmployeeFromText(FILE* pFile , LinkedList* pArrayListEmployee);
-int parser_EmployeeFromBinary(FILE* pFile , LinkedList* pArrayListEmployee);
-int parser_FindIdOfLastEmployerFromBinary(FILE* pFile);
-int parser_FindIdOfLastEmployerFromText(FILE* pFile);
-int parser_IdAlreadyLoaded(LinkedList* listaEmpleados, Employee* empleado);
-#endif // PARSER_H_INCLUDED
